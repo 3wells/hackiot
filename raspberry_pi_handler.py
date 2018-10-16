77b92b39a04d8d319bd2420acc417c3806ecc3f4
Paul Mitsui <paul.mitsui@urbanairship.com>
2018-10-16 14:25:17 -0700

    Added code to scroll display if text doesn't fit

--- raspberry_pi_handler.py
+++ raspberry_pi_handler.py
@@ -1,4 +1,5 @@
 import Adafruit_CharLCD as LCD
+import time
 
 lcd = LCD.Adafruit_CharLCD(27, 22, 25, 24, 23, 18, 16, 2, 4, False)
 
@@ -8,3 +9,8 @@ def callback(client, userdata, message):
     lcd.clear()
     lcd.set_backlight(1)
     lcd.message(message.payload)
+
+    if len(message.payload) > 16:
+        for i in range(len(message.payload) - 16):
+            time.sleep(0.5)
+            lcd.move_left()
