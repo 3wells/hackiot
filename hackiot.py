c2518cf25225118b1316a59e75b0ad8886c8da24
Paul Mitsui <paul@3wells.net>
2018-10-16 20:54:16 +0000

    Adding LCD to hackiot

--- hackiot.py
+++ hackiot.py
@@ -1,6 +1,14 @@
+import Adafruit_CharLCD as LCD
+
 from mqqtclient import MqqtClient
 from settings import Settings
 
+lcd = LCD.Adafruit_CharLCD(27, 22, 25, 24, 23, 18, 16, 2, 4,False)
+def callback(client, userdata, message):
+    print(message.payload)
+    lcd.clear()
+    lcd.set_backlight(1)
+    lcd.message(message.payload)
 
 class HackIot(object):
 
@@ -13,11 +21,14 @@ class HackIot(object):
 
         client = MqqtClient(s.device_id, s.device_endpoint, s.device_port, 'config/root-ca.pem',
                             'config/private.pem.key', 'config/certificate.pem.crt')
-        client.start_callback_loop(self.callback)
+        client.start_callback_loop(callback)
 
-    @staticmethod
-    def callback(client, userdata, message):
-        print(message.payload)
+    #@staticmethod
+    #def callback(client, userdata, message):
+    #    print(message.payload)
+    #    lcd.clear()
+    #    lcd.set_backlight(1)
+    #    lcd.message(message.payload)
 
 
 if __name__ == "__main__":
